6890ecba7a7c6284fd722c70aa944f41be83ea6e
Space2Walker <lordgreybuffer@protonmail.com>
2019-03-18 20:14:33 +0100

    New Watchbox Scrapper

--- resources/lib/sources/de/watchbox.py
+++ resources/lib/sources/de/watchbox.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import re, urllib
+import re
 import requests
 import simplejson
+
+from resources.lib.modules import cache
 from resources.lib.modules import cleantitle
 from resources.lib.modules import dom_parser
 
@@ -14,53 +16,88 @@ class source:
 
     def movie(self, imdb, title, localtitle, aliases, year):
         try:
-            title=cleantitle.getsearch(localtitle) 
-            r = requests.get("https://api.watchbox.de/v1/search/?page=1&maxPerPage=28&active=true&type=film&term=" + urllib.quote_plus(title))                        
-            data = r.json()
-            url=""
-            
-            for i in data['items']:            
-                #print "print titelname & titel",i['formatTitle'].encode("utf-8"),i['entityId'],i['productionYear'],i['seoPath'], year,type(year)
-                if int(i['productionYear']) == int(year) or int(i['productionYear']) == int(year)+1 or int(i['productionYear']) == int(year)-1:                    
-                    if cleantitle.getsearch(i['formatTitle'].encode("utf-8")) in title or title in cleantitle.getsearch(i['formatTitle'].encode("utf-8")):                        
-                        url="https://www.watchbox.de/filme/" + str(i['seoPath']) + "-" + str(i['entityId'])                        
-                        break             
-            return url
+            url = 'https://apis.justwatch.com/content/titles/de_DE/popular?body=%7B"languages":"de","content_types":["movie"],"providers":["nfx","amp","wbx","max","ntz"],"monetization_types":["flatrate","ads","free"],"page":1,"page_size":20,"query":"{}"%7D'.format(localtitle)
+            req = cache.get(requests.get, 12, url)
+            data = req.json()
+
+            # Loop through hits
+            for hit in data['items']:
+                # Compare year and title
+                if (hit['original_release_year'] == int(year)
+                        and hit['title'] == localtitle or title == hit['title']):
+
+                    for offer in hit['offers']:
+
+                        # Watchbox
+                        if offer['provider_id'] == 171:
+                            url = offer['urls']['standard_web']            
+                
+                    return url
+                    break
         except:
             return
 
     def tvshow(self, imdb, tvdb, tvshowtitle, localtvshowtitle, aliases, year):         
         try:
-            r = requests.get("https://api.watchbox.de/v1/search/?page=1&maxPerPage=28&active=true&type=serie&term=" + urllib.quote_plus(tvshowtitle))            
-            data = r.json()
-            url=""
+           
+            url = 'https://apis.justwatch.com/content/titles/de_DE/popular?body=%7B"languages":"de","content_types":["show"],"providers":["nfx","amp","wbx","max"],"monetization_types":["flatrate","ads","free"],"page":1,"page_size":20,"query":"{}"%7D'.format(localtvshowtitle)
+            req = cache.get(requests.get, 12, url)
+            data = req.json()
 
-        #print "print WB search title",title,year
-            for i in data['items']:
-                if i['type']=="serie":
-                    #print "print titel",i['formatTitle'].encode("utf-8"),tvshowtitle,localtvshowtitle#,i['entityId'],i['productionYear'],i['seoPath'], year,type(year)
-                    if int(i['productionYear']) == int(year) or int(i['productionYear']) == int(year)+1 or int(i['productionYear']) == int(year)-1:                    
-                        #print "print year match",cleantitle.getsearch(i['formatTitle'].encode("utf-8")), tvshowtitle
-                        if cleantitle.getsearch(i['formatTitle'].encode("utf-8")) in tvshowtitle.lower() or tvshowtitle.lower() in cleantitle.getsearch(i['formatTitle'].encode("utf-8")):                        
-                            url="https://www.watchbox.de/serien/" + str(i['seoPath']) + "-" + str(i['entityId'])                            
-                            break 
-            return url
+            # Loop through hits
+            for hit in data['items']:
+                # Compare year and title
+                if (hit['original_release_year'] == int(year)):
+
+                    for offer in hit['offers']:
+
+                        if offer['provider_id'] == 171:
+                            url = offer['urls']['standard_web'].rsplit("/", 2)[0]
+
+                            return url
+                            break
+
         except:
             return
 
-    def episode(self, url, imdb, tvdb, title, premiered, season, episode):        
+    def episode(self, url, imdb, tvdb, title, premiered, season, episode):  
         try:
+            if int(season) == 1:
+                s_nr = int(season)
+            if int(season) > 1:
+                s_nr = int(season) -1
             
-            html = requests.get(url)            
-            links = dom_parser.parse_dom(html.content, 'article', attrs={'class': 'row'})[0]            
-            links = dom_parser.parse_dom(links, 'section')            
-            links = [dom_parser.parse_dom(i, 'a')[0] for i in links if 'Staffel %s, Episode %s' % (season, episode) in i.content]            
-            url = "https://www.watchbox.de" + links[0].attrs['href']
-            return url
+            en_title = cleantitle.replaceUmlaute(title)
+            n_url = url + '/staffel-' + str(s_nr) + '/'
+    
+            req = cache.get(requests.get, 12, n_url)
+            data = dom_parser.parse_dom(req.text, 'a', attrs={'class': 'teaser_season-tab'}, req='href')[:-1]
+    
+            for i in data:
+                tit = cleantitle.replaceUmlaute(i.attrs['data-asset-title'].encode('utf-8'))
+    
+                if tit == en_title or tit[:-1].rstrip() == en_title:
+                    link = 'https://www.watchbox.de' + str(i.attrs['href'])
+                    break
+                         
+            if not link:
+                s_nr += 1   
+                n_url = url + '/staffel-' + str(s_nr) + '/'
+                
+                req = cache.get(requests.get, 12, n_url)
+                data = dom_parser.parse_dom(req.text, 'a', attrs={'class': 'teaser_season-tab'}, req='href')[:-1]
+            
+                for i in data:
+                    tit = cleantitle.replaceUmlaute(i.attrs['data-asset-title'].encode('utf-8'))
+    
+                    if tit == en_title or tit[:-1].rstrip() == en_title:
+                        link = 'https://www.watchbox.de' + str(i.attrs['href'])
+                        break
+    
+            return link        
         except:
             return
             
-
     def sources(self, url, hostDict, hostprDict):
         sources = []
         
@@ -68,11 +105,11 @@ class source:
             if not url:
                 return sources
             
-            html = requests.get(url)            
+            html = cache.get(requests.get, 12, url)          
             url_regex = "hls.*?(http.*?m3u8)"
             link = re.findall(url_regex, html.content)            
             link=link[0].replace("\\","")   
-            sources.append({'source': 'CDN', 'quality': 'HD', 'language': 'de', 'url': link, 'direct': True, 'debridonly': False,'info': 'Low qHD 960x540'})
+            sources.append({'source': 'CDN', 'quality': '720p', 'language': 'de', 'url': link, 'direct': True, 'debridonly': False,'info': ''})
            
             return sources
         except:
